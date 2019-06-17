c61b6161b89ee9a184fb2bde74740137021f067a
James Clarke <jaclarke2@sheffield.ac.uk>
2019-06-17 10:09:26 +0100

    Adding the new models to the summarise document

--- analysis/summarise.R
+++ analysis/summarise.R
@@ -69,4 +69,201 @@ pairs(
 )
 dev.off()
 
+###############################################################################
+## Summarise the model: hinge sex diff ----
 
+mod_name <- "hinge_sex_diff"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+
+###############################################################################
+## Summarise the model: hinge intercept age effect ----
+
+mod_name <- "hinge_intercept_age_effect"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "d_alpha_age",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+
+###############################################################################
+## Summarise the model: hinge threshold age effect ----
+
+mod_name <- "hinge_threshold_age_effect"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "d_eta_age",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+
+###############################################################################
+## Summarise the model: hinge pre-threshold slope age effect ----
+
+mod_name <- "hinge_pre_thres_slope_age_effect"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "d_beta_1_age",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+###############################################################################
+## Summarise the model: hinge intercept and threshold age effect ----
+
+mod_name <- "hinge_intercept+threshold_age_effect"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "d_alpha_age", "d_eta_age",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+
+###############################################################################
+## Summarise the model: hinge intercept and pre-threshold slope age effect ----
+
+mod_name <- "hinge_intercept+pre_thres_slope_age_effect"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "d_alpha_age", "d_beta_1_age",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+
+###############################################################################
+## Summarise the model: hinge threshold and pre-threshold slope age effect ----
+
+mod_name <- "hinge_threshold+pre_thres_slope_age_effect"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "d_eta_age", "d_beta_1_age",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+
+###############################################################################
+## Summarise the model: hinge all fixed effects age effect ----
+
+mod_name <- "hinge_age_effect_all_fixed_effects"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "d_alpha_age",
+    "d_eta_age", "d_beta_1_age",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
