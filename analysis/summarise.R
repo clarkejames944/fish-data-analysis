5502a73bb49389f591386f9c46165a87e63e60fb
James Clarke <jaclarke2@sheffield.ac.uk>
2019-07-02 09:53:48 +0100

    Some pairs plots

--- analysis/summarise.R
+++ analysis/summarise.R
@@ -267,3 +267,83 @@ pairs(
   )
 )
 dev.off()
+
+###############################################################################
+## Summarise the model: hinge sex and age effect ----
+
+mod_name <- "hinge_sex_age_diff"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male",
+    "d_alpha_age",
+    "d_eta_age", "d_beta_1_age",
+    "d_beta_2_age",
+    "eta", "d_eta_male",
+    "d_eta_age",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+
+###############################################################################
+## Summarise the model: hinge temperature effect ----
+
+mod_name <- "hinge_temp_effect"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male", "d_alpha_temp",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male", "d_beta_1_temp",
+    "d_beta_2_temp",
+    "d_eta_temp",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
+
+###############################################################################
+## Summarise the model: hinge zone effect ----
+
+mod_name <- "hinge_zone_effect"
+
+f_loc <- sub("XX", replacement = mod_name, x = "models/XX.rds")
+stan_fit <- readRDS(file = f_loc)
+
+# pairs plots
+f_loc <- sub("XX", replacement = mod_name, x = "figures/XX_pairs.pdf")
+pdf(file = f_loc, w = 15, h = 15)
+pairs(
+  stan_fit,
+  pars = c(
+    "alpha", "d_alpha_male", "d_alpha_zone",
+    "beta_1", "beta_2", 
+    "d_beta_1_male", "d_beta_2_male", "d_beta_1_zone",
+    "d_beta_2_zone",
+    "d_eta_zone",
+    "eta", "d_eta_male",
+    "sigma_fish", "sigma_year", "sigma",
+    "lp__"
+  )
+)
+dev.off()
