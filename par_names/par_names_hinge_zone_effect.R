1baa930e34e5f46c9c09a974f999f1662d77a022
James Clarke <jaclarke2@sheffield.ac.uk>
2019-07-05 16:28:07 +0100

    Adding parameter names R scripts

--- par_names/par_names_hinge_zone_effect.R
+++ par_names/par_names_hinge_zone_effect.R
@@ -1,13 +1,13 @@
 # vector of names of required parameters
 par_names <- 
   c(# intercept parameters
-    "alpha", "d_alpha_male", "d_alpha_zone",
+    "alpha", "d_alpha_male", "d_alpha_EBS", "d_alpha_ETAS", "d_alpha_WTAS",
     # threshold size
-    "eta", "d_eta_male", "d_eta_zone",
+    "eta", "d_eta_male", "d_eta_EBS", "d_eta_ETAS", "d_eta_WTAS",
     # pre-threshold slope
-    "beta_1", "d_beta_1_male", "d_beta_1_zone",
+    "beta_1", "d_beta_1_male", "d_beta_1_EBS", "d_beta_1_ETAS", "d_beta_1_WTAS",
     # post-threshold slope
-    "beta_2", "d_beta_2_male", "d_beta_2_zone",
+    "beta_2", "d_beta_2_male", "d_beta_2_EBS", "d_beta_2_ETAS", "d_beta_2_WTAS",
     # variance components
     "sigma_fish", "sigma_year", "sigma",
     # 'random' year effects
--- par_names/par_names_hinge_zone_temp_sex_diff.R
+++ par_names/par_names_hinge_zone_temp_sex_diff.R
@@ -1,13 +1,13 @@
 # vector of names of required parameters
 par_names <- 
   c(# intercept parameters
-    "alpha", "d_alpha_male", "d_alpha_temp", "d_alpha_zone",
+    "alpha", "d_alpha_male", "d_alpha_EBS", "d_alpha_ETAS", "d_alpha_WTAS", "d_alpha_temp",
     # threshold size
-    "eta", "d_eta_male", "d_eta_temp", "d_eta_zone",
+    "eta", "d_eta_male", "d_eta_EBS", "d_eta_ETAS", "d_eta_WTAS", "d_eta_temp",
     # pre-threshold slope
-    "beta_1", "d_beta_1_male", "d_beta_1_temp", "d_beta_1_zone",
+    "beta_1", "d_beta_1_male", "d_beta_1_EBS", "d_beta_1_ETAS", "d_beta_1_WTAS", "d_beta_1_temp",
     # post-threshold slope
-    "beta_2", "d_beta_2_male", "d_beta_2_temp", "d_beta_2_zone",
+    "beta_2", "d_beta_2_male", "d_beta_2_EBS", "d_beta_2_ETAS", "d_beta_2_WTAS", "d_beta_2_temp",
     # variance components
     "sigma_fish", "sigma_year", "sigma",
     # 'random' year effects
